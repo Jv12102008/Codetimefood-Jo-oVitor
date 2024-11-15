1843629118cd23152735109023afabe1f89ea67f
Jv12102008 <Jvbonfim13256@gmail.com>
2024-11-14 22:21:13 -0300

    adicionado atualização do js

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+function fazerPedido() {
+let avancar;
+if(avancar){
+    alert("selecione tres itens")
+} else{
+
+    const conteudo = document.querySelector(".conteudo")
+    conteudo.classList.add("void")
+    
+    const cont = document.querySelector(".cont")
+    cont.classList.remove("void")
+}
+    
+
+    console.log(avancar)
+}
