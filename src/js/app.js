9945a5730919c5414b4a9e7404fe1416a70db368
Jv12102008 <Jvbonfim13256@gmail.com>
2024-12-03 21:39:35 -0300

    Atualização do html, css, js foi realizada com sucesso

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,88 @@
-function fazerPedido() {
-let avancar;
-if(avancar){
-    alert("selecione tres itens")
-} else{
-
+let contador = 0
+let food 
+let valorFood
+let centavusFood
+let drink
+let valorDrink
+let centavusDrink
+let dessert
+let valorDessert
+let centavusDessert
+ function carrinho() {
     const conteudo = document.querySelector(".conteudo")
     conteudo.classList.add("void")
-    
+
     const cont = document.querySelector(".cont")
     cont.classList.remove("void")
-}
-    
+} 
 
-    console.log(avancar)
+function selectFood(botaoClicado, valor, centavus) {
+   food = botaoClicado
+   valorFood = valor
+   centavusFood = centavus
+    const selecionado = document.querySelector("#food .select")
+    if (selecionado !== null) {
+        selecionado.classList.remove("select")
+    } else {
+        contador++
+        verificarContador()
+    }
+    const seletor = "." + botaoClicado
+    const botao = document.querySelector(seletor)
+    botao.classList.add("select")
+}
+
+function selectDink(botaoClicado, valor, centavus) {
+    drink = botaoClicado
+    valorDrink = valor
+    centavusDrink = centavus
+    const selecionado = document.querySelector("#drink .select")
+    if (selecionado !== null) {
+        selecionado.classList.remove("select")
+    } else {
+        contador++
+        verificarContador()
+    }
+    const seletor = "." + botaoClicado
+    const botao = document.querySelector(seletor)
+    botao.classList.add("select")
+}
+
+function selectDessert(botaoClicado, valor, centavus) {
+    dessert = botaoClicado
+    valorDessert = valor
+    centavusDessert = centavus
+    const selecionado = document.querySelector(" #dessert .select")
+    if (selecionado !== null) {
+        selecionado.classList.remove('select')
+    } else {
+        contador++
+        verificarContador()
+    }
+
+    const seletor = "." + botaoClicado
+    const botao = document.querySelector(seletor)
+    botao.classList.add("select")
+}
+
+function verificarContador() {
+    if (contador === 3) {
+        avancarPag()
+    }
+}
+
+function avancarPag() {
+    const continuar = document.querySelector(".continuar")
+    continuar.classList.add("avancar")
+    const trocaNome = document.querySelector("footer a").innerHTML = "Avançar"
+    
+    const resumoFood = document.querySelector(".resumoFood")
+    resumoFood.innerHTML = "Your food: " + food + " R$ " + valorFood + "," + centavusFood
+
+    const resumoDrink = document.querySelector(".resumoDrink")
+    resumoDrink.innerHTML = "Your drink: " + drink + " R$ " + valorDrink + "," + centavusDrink
+
+    const resumoDessert = document.querySelector(".resumoDessert")
+    resumoDessert.innerHTML = "Your dessert: " + dessert + " R$ " + valorDessert + "," + centavusDessert
+    
 }
